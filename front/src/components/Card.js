c544a6f95f6781a4be30fb7914cf96416fdfe612
Eightyeightdays <eightyeightdays@gmail.com>
2022-07-12 18:54:56 +0200

    Front end like functions updated to remove unnecessary switch statements

--- front/src/components/Card.js
+++ front/src/components/Card.js
@@ -52,19 +52,7 @@ export default function Card(props){
         };
         fetch(`http://localhost:3001/posts/${postId}/like`, settings)
             .then(response => response.json())
-            .then(data => {
-                switch(data.message){
-                    case "LIKE ADDED":
-                    updateLikeState(data);
-                    break;
-                    case "LIKE REPLACED DISLIKE" :
-                    updateLikeState(data);
-                    break;
-                    case "LIKE REMOVED":
-                    updateLikeState(data);
-                    break;
-                }
-            })
+            .then(data => updateLikeState(data));
         }
 
     function dislikePost(){
@@ -78,19 +66,7 @@ export default function Card(props){
         };
         fetch(`http://localhost:3001/posts/${postId}/dislike`, settings)
         .then(response => response.json())
-        .then(data => {
-            switch(data.message){
-                case "DISLIKE ADDED":
-                updateLikeState(data); 
-                break;
-                case "DISLIKE REPLACED LIKE" :
-                updateLikeState(data);
-                break;
-                case "DISLIKE REMOVED":
-                updateLikeState(data);
-                break;
-            }
-        })
+        .then(data => updateLikeState(data));
     }
 
     function handleEdit(){
