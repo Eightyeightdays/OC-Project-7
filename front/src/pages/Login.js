fd318d36dee38a7835de8b67d700d6ae4fdecf20
Eightyeightdays <eightyeightdays@gmail.com>
2022-07-15 13:46:00 +0200

    Login/Signup UI good enough

--- front/src/pages/Login.js
+++ front/src/pages/Login.js
@@ -5,12 +5,12 @@ export default function LoginAndSignUp(){
     const [error, setError] = useState([]);
     const [existingUser, setExistingUser] = useState(true);
     const [buttonLabel, setButtonLabel] = useState("Already have an account?");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [emailField, setEmailField] = useState("");
+    const [passwordField, setPasswordField] = useState("");
     const navigate = useNavigate();
     
-    function displayErrorMessage(err){
-        setError(err);
+    function displayErrorMessage(array){
+        setError(array);
         const errorElement = document.getElementById("error");
         errorElement.className="visible";
         setTimeout(()=>{
@@ -18,9 +18,18 @@ export default function LoginAndSignUp(){
             5000);
     }
 
+
     function handleLogin(){
         const loginForm = document.getElementById("loginForm");
         const user = Object.fromEntries(new FormData(loginForm).entries());
+        let errorMessage =[];
+
+        if(!emailField || !passwordField){
+            errorMessage.push("LOGIN DETAILS CANNOT BE EMPTY");
+            displayErrorMessage(errorMessage);
+            return;
+        }
+
         const settings = {
         method: "POST",
         credentials: "include",
@@ -39,20 +48,23 @@ export default function LoginAndSignUp(){
                 if(data.admin){ 
                     console.log("ADMIN SIGNED IN");
                     navigate("/home");
-                    console.log("DIDN'T NAVIGATE HOME")
                 }
                 navigate("/home"); 
             }else{
-                displayErrorMessage("LOGIN DETAILS INCORRECT");
+                errorMessage.push("LOGIN DETAILS INCORRECT")
+                displayErrorMessage(errorMessage);
+                return;
             }
         })
     }
 
     function handleInputs(event){
+        const errorElement = document.getElementById("error");
+        errorElement.className = errorElement.className.replace("visible", "");
         if(event.target.type === "text"){
-            setEmail(event.target.value);
+            setEmailField(event.target.value);
         }else{
-            setPassword(event.target.value);
+            setPasswordField(event.target.value);
         }
     }
 
@@ -67,7 +79,7 @@ export default function LoginAndSignUp(){
         schema.is().max(15, "Password must be a maxiumum of 15 characters");
         schema.has().uppercase(2, "Password must contain at least 2 uppercase letters");
         schema.has().symbols(2, "Password must contain at least 2 symbols");
-    
+       
         let passwordCheck = schema.validate(password);
         let emailCheck = emailValidator.validate(email); 
         let errorMessage =[];
@@ -79,8 +91,7 @@ export default function LoginAndSignUp(){
             console.log("PASSWORD & EMAIL ERROR: " + errorMessage);
             displayErrorMessage(errorMessage);
             return;
-        }
-        else if(passwordCheck !== true){
+        }else if(passwordCheck !== true){
             passwordCheck = schema.validate(password, {details: true});
             passwordCheck.forEach(error => errorMessage.push(error.message));
             console.log("PASSWORD ERROR: " + errorMessage); 
@@ -97,6 +108,9 @@ export default function LoginAndSignUp(){
 
     function handleSignUp(){
         const user = checkNewUser();
+        if(user===undefined){
+            return;
+        }
 
         const settings = {
         method: "POST",
@@ -124,8 +138,10 @@ export default function LoginAndSignUp(){
     
     function changeUi(){
         setExistingUser(!existingUser);
-        setEmail("");
-        setPassword("");
+        setEmailField("");
+        setPasswordField("");
+        const errorElement = document.getElementById("error");                      ///  make global if possible
+        errorElement.className = errorElement.className.replace("visible", "");             
         if(existingUser){
             setButtonLabel("Sign up");
             setError("");
@@ -143,8 +159,8 @@ export default function LoginAndSignUp(){
                 <>
                     <h2>Welcome back!</h2>
                     <form id="loginForm">
-                        <input type="text" name="email" placeholder="Email address" onChange={event=>handleInputs(event)} value={email} />
-                        <input type="password" name="password" placeholder="Password" maxLength="15" onChange={event=>handleInputs(event)} value={password} />
+                        <input type="text" name="email" placeholder="Email address" onChange={event=>handleInputs(event)} value={emailField} />
+                        <input type="password" name="password" placeholder="Password" maxLength="15" onChange={event=>handleInputs(event)} value={passwordField} />
                     </form> 
                     <button className="loginButton" type="submit" onClick={handleLogin}>LOGIN</button>
                 </> 
@@ -152,18 +168,18 @@ export default function LoginAndSignUp(){
                <>
                     <h2>Welcome!</h2>
                     <form id="signUpForm">
-                        <input type="text" name="email" placeholder="Email address" onChange={event=>handleInputs(event)} value={email} />
-                        <input type="password" name="password" placeholder="Password" maxLength="15" onChange={event=>handleInputs(event)} value={password} />
+                        <input type="text" name="email" placeholder="Email address" onChange={event=>handleInputs(event)} value={emailField} />
+                        <input type="password" name="password" placeholder="Password" maxLength="15" onChange={event=>handleInputs(event)} value={passwordField} />
                     </form> 
                     <button className="loginButton" type="submit" onClick={handleSignUp}>SIGN UP AND LOG IN</button>
                 </>
                 }
                 <button className="selectButton" type="button" onClick={changeUi}>{buttonLabel}</button> 
-                {error && <div id="error">
+                <div id="error">
                     <ul>
-                        {error.map(err=> <li>{err}</li>)}
+                        {error && error.map((err, index)=> <li key={index}>{err}</li>)}
                     </ul>
-                </div>}
+                </div>
             </div>
         </>
     )
